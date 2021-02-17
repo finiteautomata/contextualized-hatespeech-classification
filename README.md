562742c38609b1029c963cd4c959bf890c55cb65
Juan Manuel Pérez <jmperez.85@gmail.com>
2021-02-17 10:52:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hatespeech Classification in News
+
+1. Get the dataset and put it under `data/`
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hatespeech-classification',
+    version='0.0.1',
+    packages=find_packages(),
+    test_suite="tests",
+)
