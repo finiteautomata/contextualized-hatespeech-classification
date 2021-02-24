5c62b0325362e99bf17bbba0bca6e04f95b0a3eb
Juan Manuel Pérez <jmperez.85@gmail.com>
2021-02-24 13:57:54 -0300

    Create script to generate dataset

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Hatespeech Classification in News
 
 1. Get the dataset and put it under `data/`
+
+2. Generate dataset
+
+```
+python bin/create_dataset.py data/articles.json data/comments.json --train_path data/train.json --test_path data/test.json
+```
---
+++ bin/create_dataset.py
@@ -0,0 +1,74 @@
+import json
+import fire
+from sklearn.model_selection import train_test_split
+from tqdm.auto import tqdm
+
+categories = [
+    "WOMEN", "LGBTI", "RACISM", "CLASS",
+    "POLITICS", "DISABLED", "APPEARANCE", "CRIMINAL",
+]
+
+def process_comment(raw_comment):
+    """
+    Process raw comment
+    """
+    ret = {
+        "text": raw_comment["text"],
+        "is_hateful": int(len(raw_comment['HATE']) >= 2)
+    }
+    for cat in categories + ["calls"]:
+        ret[cat] = 0
+
+    if ret["is_hateful"]:
+        ret["calls"] = int(len(raw_comment['CALLS']) >= 2)
+
+        for category in categories:
+            ret[category] = int(len(raw_comment[category]) > 0)
+
+    return ret
+
+
+def create_dataset(articles_path, comments_path, train_path, test_path, train_size=0.8, random_state=2021):
+    """
+    Creates train and test dataset
+    """
+
+    with open(articles_path) as f:
+        raw_articles = json.load(f)
+
+    with open(comments_path) as f:
+        raw_comments = json.load(f)
+
+    print(f"We have {len(raw_articles)} articles and {len(raw_comments)} comments")
+    articles = {art['tweet_id']:art for art in raw_articles}
+
+    for art in articles.values():
+        art["comments"] = []
+
+    counts = []
+
+
+    for comment in tqdm(raw_comments):
+        tweet_id = comment["article_id"]
+        article = articles[tweet_id]
+        article["comments"].append(
+            process_comment(comment)
+        )
+
+
+    train_articles, test_articles = train_test_split(list(articles.values()),
+        train_size=train_size, random_state=random_state
+    )
+
+    print(f"Train Articles: {len(train_articles)}")
+    print(f"Test Articles: {len(test_articles)}")
+
+    with open(train_path, "w") as f:
+        json.dump(train_articles, f)
+
+    with open(test_path, "w") as f:
+        json.dump(test_articles, f)
+
+
+if __name__ == '__main__':
+    fire.Fire(create_dataset)
