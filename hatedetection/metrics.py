cf62f1420649169a5e37600c331fd5b8f8e3d5e9
Juan Manuel Pérez <jmperez.85@gmail.com>
2021-03-18 10:15:36 -0300

    Add metrics module

---
+++ hatedetection/metrics.py
@@ -0,0 +1,19 @@
+"""
+Compute metrics during training
+"""
+from sklearn.metrics import precision_recall_fscore_support, accuracy_score
+
+def compute_hate_metrics(pred):
+    """
+    Compute metrics for Trainer
+    """
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='macro')
+    acc = accuracy_score(labels, preds)
+    return {
+        'accuracy': acc,
+        'f1': f1,
+        'precision': precision,
+        'recall': recall
+    }
